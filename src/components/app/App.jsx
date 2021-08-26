a122ec4cc486b837891edb60eab8335093f94c58
David Delgadillo <daviddel.731@gmail.com>
2021-08-26 13:06:33 -0700

    Added styling

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,32 @@
+/* eslint-disable max-len */
 import React from 'react';
 import DisplaySequence from '../display/DisplaySequence';
+import styled from 'styled-components';
 
 export default function App() {
-  return <DisplaySequence />;
+  return (
+    <>
+      <h1
+        style={{ color: 'white', fontSize: '70px', padding: '1rem', fontFamily: 'Pacifico',
+          textShadow: '3px 3px 5px black'
+        }}
+      >
+        Chordial</h1>
+      <AppStyled>
+        <DisplaySequence />
+      </AppStyled>
+    </>
+  ) ;
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  
+}
+
+`;
+
