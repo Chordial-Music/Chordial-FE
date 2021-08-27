0647baec95760df7783d27fa6539214b1e814707
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-27 13:50:11 -0700

    Login form skeleton working w/o backend

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import login 
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const login = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //login(username, password);
+  };
+
+  const handleChange = ({ target }) => {
+    if (target.name === 'username') setUsername(target.value);
+    if (target.name === 'password') setPassword(target.value);
+  };
+
   return (
-    <div>
-      Login
-    </div>
+    <>
+      hello
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username</label>
+        <input
+          id='username'
+          type='username'
+          name='username'
+          placeholder='username'
+          value={username}
+          onChange={handleChange}
+        />
+
+        <label htmlFor='password'>Password</label>
+        <input
+          id='password'
+          type='password'
+          name='password'
+          placehowlder='password'
+          value={password}
+          onChange={handleChange}
+        />
+
+        <button>Login</button>
+      </form>
+    </>
   )
-}
+};
