b33f4ed28c4e1bf37e2d4766e26c0b807c0ff600
Kalan Prudhomme <cleaver9@gmail.com>
2021-09-17 16:05:36 -0700

    media queries complete for side menu, modals, main page, about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -133,4 +133,85 @@ const AboutStyled = styled.div`
         }
     }
   }
+
+  @media only screen and (max-width: 1000px) {
+    &::-webkit-scrollbar {
+      display: none;
+    }
+      background-color: #c764ee55;
+      width: 97%;
+      height: 100%;
+      border-radius: 40px;
+      box-shadow: 0px 0px 15px 0px black;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      align-items: center;
+      margin: 10px;
+  
+      .description {
+        width: 100%;
+        text-align: center;
+        padding: 2rem 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        h1 {
+          font-family: 'Concert One';
+          font-size: 2rem;
+          padding: 2rem 0;
+        }
+    
+        p {
+          font-family: 'Righteous';
+          padding: 1rem;
+          text-align: center;
+          font-size: 1.5rem;
+          width: 90%;
+          line-height: 1.5;
+          background-color: #ffffff92;
+          border-radius: 1rem;
+        }
+      }
+    
+      .devs-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+        width: 100%;
+        padding-bottom: 2rem;
+
+        .dev {
+          width: 350px;
+          text-align: center;
+          padding: 1rem;
+          margin: 10px;
+            background-color: #85affd9e;
+            border-radius: 15px;
+            box-shadow: 0px 0px 10px 0px black;
+    
+            .icon {
+              margin: 0 0.6rem;
+              font-size: 2rem;
+              transition: all ease-in-out 0.15s;
+              &:hover {
+                color: #f6d569;
+              }
+            }
+            img {
+              
+              height: 250px;
+              width: 250px;
+              object-fit: contain;
+            }
+    
+            p {
+              padding: 1rem;
+            }
+        }
+      }
+  }
 `;
--- src/components/common/AlertModal.js
+++ src/components/common/AlertModal.js
@@ -56,10 +56,17 @@ const BackdropStyled = styled.div`
   height: 100vh;
   z-index: 1001;
   background: rgba(0, 0, 0, 0.75);
+
+  @media only screen and (max-width: 900px) {
+    width: 100%;
+    height: 100vh;
+    z-index: 1001;
+    background: rgba(0, 0, 0, 0.75);
+  }  
 `;
 
 const ModalStyled = styled.section`
-  position: fixed;
+  position: absolute;
   top: 30vh;
   left: 30%;
   width: 40%;
@@ -97,5 +104,46 @@ const ModalStyled = styled.section`
     &:hover {
       background-color: lightskyblue;
     }
+  }
+
+  @media only screen and (max-width: 1000px) {
+  position: absolute;
+  height: 400px;
+  z-index: 1010;
+  overflow: hidden;
+  background-color: white;
+  opacity: 0.8;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+  .header {
+    font-family: 'Concert One';
+    font-size: 2rem;
+    text-align: center;
+    padding: 5px;
+  }
+
+  .modal-text {
+    font-size: 1.3rem;
+    text-align: center;
+    padding: 0 2rem;
+  }
+
+  .confirm-btn {
+    background-color: transparent;
+    border: 1px solid black;
+    outline: none;
+    border-radius: 6px;
+    padding: 1rem;
+    cursor: pointer;
+    transition: all ease-in-out 0.15s;
+
+    &:hover {
+      background-color: lightskyblue;
+    }
+  }
   }
 `;
